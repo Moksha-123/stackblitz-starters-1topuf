2684e2cf79aad67a5e3372d0fecc4edb0c8ebaf5
Moksha-123 <141109376+Moksha-123@users.noreply.github.com>
2023-08-14 12:30:31 +0530

    1 file modified

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -11,7 +11,7 @@ export class HeroDetailsComponent implements OnInit {
   @Input() selectedHero!:Hero;
 
   constructor() { 
-    console.log(this.selectedHero);
+    // console.log(this.selectedHero);
   }
 
   ngOnInit() {
